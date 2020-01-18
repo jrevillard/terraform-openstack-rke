6079c9732963cf19e16b18263842d9c9bd0140a1
Jérôme Revillard <jrevillard@users.noreply.github.com>
2020-01-18 14:12:52 +0100

    Disable port security

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,7 +1,7 @@
 resource "openstack_networking_network_v2" "nodes_net" {
   name                  = var.network_name
   admin_state_up        = "true"
-  port_security_enabled = "true"
+  port_security_enabled = "false"
 }
 
 resource "openstack_networking_subnet_v2" "nodes_subnet" {
